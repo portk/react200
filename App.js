c18806ebeff3b5970ae4ecb05c6f44b3fc14dcb9
Gavin <kunhee0713@gmail.com>
2023-11-20 20:07:17 +0900

    ex 63
    ex 63 Callback Function

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import AxiosPost from './R062_AxiosPost';
+import CallbackFunc from './R064_CallbackFunc';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <AxiosPost/>
+      <CallbackFunc/>
     </div>
   );
 }
