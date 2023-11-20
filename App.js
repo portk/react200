340bc637d830618aedaed501f1edb559b95f4da2
Gavin <kunhee0713@gmail.com>
2023-11-20 17:47:40 +0900

    ex30
    ex30 FunctionComponent

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ShallowEqual from './R029_ShallowEqual';
+import FunctionComponent from './R030_FunctionComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ShallowEqual/>
+      <FunctionComponent/>
     </div>
   );
 }
