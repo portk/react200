025bebd56dc94975ca8713d7eef70bd43b9077f5
Gavin <kunhee0713@gmail.com>
2023-11-20 19:26:09 +0900

    ex 59
    ex59 fetch get // Some examples (that from before commit to this commit) was passed. Because they are request just type for using external library.

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactstrapBadge from './R035_ReactstrapBadge';
-import "bootstrap/dist/css/bootstrap.css"
+import FetchGet from './R059_FetchGet';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapBadge/>
+      <FetchGet/>
     </div>
   );
 }
