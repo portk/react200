0198be1afc821ab8351133d27a5cf6153c270c08
Gavin <kunhee0713@gmail.com>
2023-11-20 19:54:21 +0900

    ex62
    ex62 axios post

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import AxiosGet from './R061_AxiosGet';
+import AxiosPost from './R062_AxiosPost';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <AxiosGet/>
+      <AxiosPost/>
     </div>
   );
 }
