1a46aa5da88507af56fde2a223c52fda099b832b
Desktop <kunhee0713@gmail.com>
2023-11-22 00:09:58 +0900

    ex 74
    ex 74 currying

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactRef from './R073_ReactRef';
+import Curring from './R074_ReactCurrying';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactRef/>
+      <Curring/>
     </div>
   );
 }
