331c85a4b22393d50de6717a3b5880ade3af6519
Gavin <kunhee0713@gmail.com>
2023-11-22 17:46:59 +0900

    ex 76
    ex 76 ContextApi 사용하기 ({Provider, Consumer} = React.createContext())

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ReactHoc from './R075_ReactHoc';
+import ContextApi from './R076_ContextApi';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactHoc name="react200"/>
+      <ContextApi/>
     </div>
   );
 }
