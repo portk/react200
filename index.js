7005370c0e24a2389517ce12fbbcd90b826d15b5
Gavin <kunhee0713@gmail.com>
2023-11-22 19:07:11 +0900

    ex 78
    ex78 리듀서로 스토어 생성하기

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import reducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 78~80번의 실행결과는 80번에서 확인한다.
+// redux는 컨텍스트처럼 데이터를 필요한 컴포넌트에서만 요청해 사용할 수 있다.
+// 컨텍스트는 부모 컴포넌트에서 생성한 데이터에 모든 자식 컴포넌트에서 접근할 수 있다.
+// 리덕스는 컴포넌트 외부의 스토어라는 곳에서 관리하여 컴포넌트의 위치와 상관없이 스토어에 접근해 데이터를 사용하고 변경할 수 있다.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducers);
+
+const listener = ()=> {
+  ReactDOM.render(
+    <App store={store}/>,
+    document.getElementById("root")  
+  );
+};
+
+store.subscribe(listener);
+listener();
